e109c37fc40afd82a0e0928f5c3dd1d6444f5786
shill <2714606709@qq.com>
2023-11-19 22:37:03 +0800

    幂等物品的设计

--- gran-chat-server/src/main/java/com/shill/gran/common/user/dao/UserBackpackDao.java
+++ gran-chat-server/src/main/java/com/shill/gran/common/user/dao/UserBackpackDao.java
@@ -1,13 +1,22 @@
 package com.shill.gran.common.user.dao;
 
+import com.shill.gran.common.framworkEntiry.enums.IdempotentEnum;
 import com.shill.gran.common.framworkEntiry.enums.YesOrNoEnum;
+import com.shill.gran.common.user.domain.entity.ItemConfig;
 import com.shill.gran.common.user.domain.entity.UserBackpack;
+import com.shill.gran.common.user.domain.enums.ItemTypeEnum;
 import com.shill.gran.common.user.mapper.UserBackpackMapper;
 import com.shill.gran.common.user.service.IUserBackpackService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.shill.gran.common.user.service.cache.ItemCache;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * <p>
@@ -19,6 +28,14 @@ import java.util.List;
  */
 @Service
 public class UserBackpackDao extends ServiceImpl<UserBackpackMapper, UserBackpack> implements IUserBackpackService {
+    @Autowired
+    private UserBackpackDao userBackpackDao;
+    @Autowired
+    private ItemCache itemCache;
+
+    @Autowired
+    private RedissonClient redissonClient;
+
     public Integer getCountByValidItemId(Long uid,Long itemId){
         return lambdaQuery().eq(UserBackpack::getUid,uid)
                 .eq(UserBackpack::getItemId,itemId)
@@ -47,5 +64,46 @@ public class UserBackpackDao extends ServiceImpl<UserBackpackMapper, UserBackpac
 
     public UserBackpack getByIdp(String idempotent) {
         return lambdaQuery().eq(UserBackpack::getIdempotent, idempotent).one();
+    }
+
+    @Override
+    public void acquireItem(Long uid, Long itemId, IdempotentEnum idempotentEnum, String businessId) {
+        //组装幂等号
+        String idempotent = getIdempotent(itemId, idempotentEnum, businessId);
+        this.doAcquireItem(uid, itemId, idempotent);
+    }
+    private void doAcquireItem(Long uid, Long itemId, String idempotent) {
+        RLock lock = redissonClient.getLock(idempotent);
+        lock.tryLock();
+        try {
+            UserBackpack userBackpack = userBackpackDao.getByIdp(idempotent);
+            //幂等检查
+            if (Objects.nonNull(userBackpack)) {
+                return;
+            }
+            //业务检查
+            ItemConfig itemConfig = itemCache.getById(itemId);
+            if (ItemTypeEnum.BADGE.getType().equals(itemConfig.getType())) {//徽章类型做唯一性检查
+                Integer countByValidItemId = userBackpackDao.getCountByValidItemId(uid, itemId);
+                if (countByValidItemId > 0) {//已经有徽章了不发
+                    return;
+                }
+            }
+            //发物品
+            UserBackpack insert = UserBackpack.builder()
+                    .uid(uid)
+                    .itemId(itemId)
+                    .status(YesOrNoEnum.NO.getStatus())
+                    .idempotent(idempotent)
+                    .build();
+            userBackpackDao.save(insert);
+        }catch (Exception e){
+            lock.unlock();
+        }
+
+    }
+
+    private String getIdempotent(Long itemId, IdempotentEnum idempotentEnum, String businessId) {
+        return String.format("%d_%d_%s", itemId, idempotentEnum.getType(), businessId);
     }
 }
--- gran-chat-server/src/main/java/com/shill/gran/common/user/service/IUserBackpackService.java
+++ gran-chat-server/src/main/java/com/shill/gran/common/user/service/IUserBackpackService.java
@@ -1,5 +1,6 @@
 package com.shill.gran.common.user.service;
 
+import com.shill.gran.common.framworkEntiry.enums.IdempotentEnum;
 import com.shill.gran.common.user.domain.entity.UserBackpack;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,13 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IUserBackpackService extends IService<UserBackpack> {
 
+    /**
+     * 用户获取一个物品
+     *
+     * @param uid            用户id
+     * @param itemId         物品id
+     * @param idempotentEnum 幂等类型
+     * @param businessId     上层业务发送的唯一标识
+     */
+    void acquireItem(Long uid, Long itemId, IdempotentEnum idempotentEnum, String businessId);
 }
